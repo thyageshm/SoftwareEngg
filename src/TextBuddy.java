9a056a19ee049a30f898c5a58a405b0bf1ad2e92
Thyagesh Manikandan <thyageshm@gmail.com>
2013-08-23 00:48:32 +0800

    changes in fileinitializer, save and autosave
    file exception handling shifted to main
    reintroduced close and finalize methods with the _hasClosed variable
    The autosave now uses a LinkedBlockingQueue that is populated by the
    main thread

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -4,8 +4,10 @@
  * - if an unknown command is given, let the user know that it is an unknown command
  * - if the given files exists, load the data from it so that an initial display call can be made
  * - if the given file has problems or the program is unable to create a file with the given name, exit gracefully
+ * - if "display" is followed by any other characters, ignore them
  */
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.BufferedReader;
@@ -46,89 +48,47 @@ public class TextBuddy implements Runnable {
 	private Thread _autoSaver = null;
 	private LinkedBlockingQueue<LogDataHolder> _unsavedChanges = new LinkedBlockingQueue<LogDataHolder>();
 	private boolean _hasChanged = false;
+	private boolean _hasNotClosed = true;
+	private boolean _hasEnded = false;
 
-	public TextBuddy(String _fileName) {
+	public TextBuddy(String _fileName) throws IOException,
+			FileNotFoundException, SecurityException {
 		this._fileName = _fileName;
-		boolean NoProblems = this.initialiseTextBuddy();
-		if(NoProblems)
-		{
-			this._autoSaver = new Thread(this, "autoSaver");
-			_autoSaver.setPriority(Thread.MAX_PRIORITY);
-			
-		}
+		this.initialiseFileHandling();
+		this._autoSaver = new Thread(this, "autoSaver");
+		_autoSaver.setPriority(Thread.MAX_PRIORITY);
 	}
 
 	public void start() {
 		_autoSaver.start();
 		this.interact();
-
-		if (_autoSaver != null) {
-			try {
-				_autoSaver.interrupt();
-				_autoSaver.join();
-			} catch (InterruptedException ie) {
-				out.println("The main program thread was affected by the _autoSaver thread!");
-			} catch (SecurityException se) {
-				out.println("The main thread has lost access to the _autoSaver thread!");
-			}
-		}
-		out.println("close called...");
 		this.close();
-
 	}
 
-	/**
-	 * The following function handles all the file related initializations
-	 */
-	
-	public boolean initialiseTextBuddy() {
-		File givenFile = new File(this._fileName);
-		boolean hasNoErrors = true;
-		try {
-			if (givenFile.exists()) {
-				if (!givenFile.isFile()) {
-					out.println("Given parameter is not a file!");
-					hasNoErrors = false;
-				} else if (!givenFile.canRead()) {
-					out.println("The program does not have enough permissions to read the file!");
-					hasNoErrors = false;
-				} else if (!givenFile.canWrite()) {
-					out.println("The program does not have enough permissions to write into the file!");
-					hasNoErrors = false;
-				}
-				
-				if(hasNoErrors)
-				{
-					BufferedReader bufFileReader = new BufferedReader(new FileReader(
-							givenFile));
-		
-					String lineReader = "";
-					while ((lineReader = bufFileReader.readLine()) != null) {
-						this._localCopyOfFileData.add(lineReader);
-					}
-					bufFileReader.close();
-				}
-				else
-				{
-					return false;
-				}
-			} else {
-				if (!givenFile.createNewFile()) {
-					out.println("Could not create a new file in the disk! Permissions not given!");
-					hasNoErrors = false;
-				}
-			}
-			
-			this._bufFileWriter = new BufferedWriter(new FileWriter(givenFile));
-			return true;
+	/* The following function handles all the file related initializations */
+	public void initialiseFileHandling() throws IOException, SecurityException,
+			FileNotFoundException {
 
-		} catch (IOException io) {
-			out.println("An input-output exception occurred while trying to open/read from/write into the file given!");
-			return false;
-		} catch (SecurityException se) {
-			out.println("You do not have enough permissions to write into the file!");
-			return false;
+		File givenFile = new File(this._fileName);
+
+		if (givenFile.exists()) {
+			BufferedReader bufFileReader = new BufferedReader(new FileReader(
+					givenFile));
+
+			String lineReader = "";
+			while ((lineReader = bufFileReader.readLine()) != null) {
+				this._localCopyOfFileData.add(lineReader);
+			}
+			bufFileReader.close();
+		} else {
+			if (!givenFile.createNewFile()) {
+				out.println("Could not create a new file in the disk! Permissions not given!");
+			}
 		}
+
+		this._bufFileWriter = new BufferedWriter(new FileWriter(givenFile));
+		return;
+
 	}
 
 	public void interact() {
@@ -147,7 +107,6 @@ public class TextBuddy implements Runnable {
 				out.println(delete(_inputStream.nextLine().trim()));
 				break;
 			case "display":
-				out.println("'display a' not resolved!");
 				out.println(this.display());
 				break;
 			case "clear":
@@ -171,7 +130,7 @@ public class TextBuddy implements Runnable {
 		if (errorMessage.isEmpty()) {
 			_localCopyOfFileData.add(toAdd);
 			this._hasChanged = true;
-			this.logChange(Operation.ADD, toAdd);
+			this.logTheChange(Operation.ADD, toAdd);
 			return "added to " + this._fileName + ": \"" + toAdd + "\"";
 		} else
 			return errorMessage;
@@ -191,7 +150,7 @@ public class TextBuddy implements Runnable {
 
 		if (errorMessage.isEmpty()) {
 			this._hasChanged = true;
-			this.logChange(Operation.DELETE, strIndex);
+			this.logTheChange(Operation.DELETE, strIndex);
 			return "deleted from " + this._fileName + ": \""
 					+ _localCopyOfFileData.remove(index - 1) + "\"";
 		} else
@@ -199,7 +158,7 @@ public class TextBuddy implements Runnable {
 	}
 
 	public String clear() {
-		this.logChange(Operation.CLEAR, "");
+		this.logTheChange(Operation.CLEAR, "");
 		this._localCopyOfFileData.clear();
 		this._hasChanged = true;
 		return "all content deleted from " + this._fileName;
@@ -218,7 +177,7 @@ public class TextBuddy implements Runnable {
 		return returnString;
 	}
 
-	private void logChange(Operation operation, String parameter) {
+	private void logTheChange(Operation operation, String parameter) {
 		try {
 			this._unsavedChanges.put(new LogDataHolder(operation, parameter));
 		} catch (InterruptedException ie) {
@@ -228,18 +187,25 @@ public class TextBuddy implements Runnable {
 	}
 
 	public void save() {
+		out.println("save called!");
 		try {
-			out.println("save called");
 			LogDataHolder logData;
 
 			while ((logData = this._unsavedChanges.poll()) != null) {
 				switch (logData.operation) {
 				case ADD:
-
+					this._bufFileWriter.write(logData.parameter);
+					this._bufFileWriter.newLine();
+					this._bufFileWriter.flush();
 					break;
 				case DELETE:
+					int lineNumber = Integer.parseInt(logData.parameter);
+					this.deleteLineFromFile(lineNumber);
 					break;
 				case CLEAR:
+					this._bufFileWriter.close();
+					this._bufFileWriter = new BufferedWriter(new FileWriter(
+							new File(this._fileName)));
 					break;
 				}
 			}
@@ -253,20 +219,72 @@ public class TextBuddy implements Runnable {
 		}
 	}
 
-	public void run() {
+	private boolean deleteLineFromFile(int lineNumber) {
 		try {
-			while (true) {
-				Thread.sleep(100);
+			File tempFile = File.createTempFile("temp_TextBuddy", "");
+			File oldFile = new File(this._fileName);
+			BufferedWriter bufTempWriter = new BufferedWriter(new FileWriter(
+					tempFile));
+			BufferedReader bufTempReader = new BufferedReader(new FileReader(
+					oldFile));
+
+			for (int i = 0; i < lineNumber; i++) {
+				bufTempWriter.write(bufTempReader.readLine());
+			}
+
+			String tempLineReader;
+
+			// to skip that particular line
+			bufTempReader.readLine();
+
+			while ((tempLineReader = bufTempReader.readLine()) != null) {
+				bufTempWriter.write(tempLineReader);
+			}
+			bufTempReader.close();
+			this._bufFileWriter.close();
+			bufTempWriter.close();
+
+			oldFile.delete();
+			tempFile.renameTo(oldFile);
+			this._bufFileWriter = new BufferedWriter(new FileWriter(oldFile));
+		} catch (IOException io) {
+			out.println("The file can not longer be accessed to be saved! Please restart the program.");
+			return false;
+		} catch (SecurityException se) {
+			out.println("A temporary file could not be created to save the file");
+			return false;
+		}
+		return true;
+	}
+
+
+	public void run() {
+
+		try {
+			while (!this._hasEnded) {
+				Thread.sleep(5000);
 				this.save();
 			}
 		} catch (InterruptedException ie) {
-			out.println("interrupted!");
+			out.println("Autosaver was interrupted!");
 			return;
 		}
 
 	}
 
+	
 	public void close() {
+		if (_autoSaver != null) {
+			try {
+				this._hasEnded = true;
+				_autoSaver.join();
+			} catch (InterruptedException ie) {
+				out.println("The main program thread was affected by the _autoSaver thread!");
+			} catch (SecurityException se) {
+				out.println("The main thread has lost access to the _autoSaver thread!");
+			}
+		}
+
 		if (this._hasChanged) {
 			this.save();
 		}
@@ -280,12 +298,27 @@ public class TextBuddy implements Runnable {
 		} catch (IOException io) {
 			out.println("An error occurred while trying to close the file hander!");
 		}
+		this._hasNotClosed = false;
+	}
+
+	public void finalise() {
+		if (this._hasNotClosed) {
+			this.close();
+		}
 	}
 
 	public static void main(String[] args) {
 		if (args.length > 0) {
-			TextBuddy Textbuddy = new TextBuddy(args[0]);
-			Textbuddy.start();
+			try {
+				TextBuddy Textbuddy = new TextBuddy(args[0]);
+				Textbuddy.start();
+			} catch (FileNotFoundException fe) {
+				out.println("Given file could not be accessed!");
+			} catch (IOException io) {
+				out.println("An input-output exception occurred while trying to open/read from/write into the file given!");
+			} catch (SecurityException se) {
+				out.println("The program not have enough permissions to access/write into the file!");
+			}
 		} else {
 			out.println("No file given!");
 		}
